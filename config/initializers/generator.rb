d7dc8b91fad47eeb08a8547e46569c9e143bcad7
Tomohisa Kuranari <m.qureana@gmail.com>
2017-12-16 14:35:48 +0900

    不要コードの生成を防ぐようにgeneratorの設定を変更

---
+++ config/initializers/generator.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.template_engine false
+  g.test_unit false
+  g.test_framework :rspec, view_specs: false, helper_specs: false
+end
