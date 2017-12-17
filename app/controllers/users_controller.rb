82303df3097fc84e9e49c5b117710633102ae88d
Tomohisa Kuranari <m.qureana@gmail.com>
2017-12-17 17:48:35 +0900

    新規登録エンドポイントの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate, only: :create
-
-  def show
-    user = User.find(params[:id])
-    render json: user
-  end
-
   def create
     user = User.new(user_params)
 
