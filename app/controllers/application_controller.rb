dca1b203762f8409028af068575d304b24e02e57
Tomohisa Kuranari <m.qureana@gmail.com>
2017-12-18 02:18:53 +0900

    404のレスポンスを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   def current_user
     @current_user
@@ -25,5 +26,9 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     render json: { message: 'You are not authorized to perform this action.' }, status: :unauthorized
+  end
+
+  def render_404
+    render json: { message: 'Not found' }, status: :not_found
   end
 end
