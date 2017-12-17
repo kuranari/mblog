0e3a72d418f6f4770f4e3dd406e045fff0a2e936
Tomohisa Kuranari <m.qureana@gmail.com>
2017-12-17 18:14:44 +0900

    has_many throughの関係を定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+
   validates :title, presence: true
   validates :body, presence: true
 end
